c01438e36ade407265b7d41cd37d0c62b2b00a4b
giangtpu <giangtpu@gmail.com>
2023-02-15 16:09:09 +0700

    read file and response to client

--- 01-node-farm/app.js
+++ 01-node-farm/app.js
@@ -1,5 +1,8 @@
 const http = require('http');
+const fs = require('fs');
 
+const data = fs.readFileSync("./dev-data/data.json","utf-8");
+const jsonData = JSON.parse(data);  //convert text to Js object
 
 //request - response
 const server = http.createServer((req, res) => {
@@ -9,7 +12,24 @@ const server = http.createServer((req, res) => {
         res.end('THIS IS OVERVIEW PAGE');
     }else if (pathName === '/product'){
         res.end('THIS IS PRODUCT PAGE')
-    }else{
+    }else if (pathName === '/api'){
+        //Method 1: read the data every time request come in
+        // fs.readFile("./dev-data/data.json","utf-8", (err, data) => {
+        //     const jsonData = JSON.parse(data);  //convert text to Js object
+        //     console.log(jsonData)
+        //
+        //     res.writeHead(200,{
+        //         'Content-type': 'application/json',
+        //     })
+        //     res.end(data);
+        // });
+
+        res.writeHead(200,{
+            'Content-type': 'application/json',
+        })
+        res.end(data);
+
+    } else{
         res.writeHead(404,{
             'Content-type': 'text/html',
             'my-header': 'abc'
